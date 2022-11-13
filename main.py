cbacb90ccc882600c3fd37c49f1d2845d83d1188
Pavel Jahoda <pjahoda6@gmail.com>
2022-11-13 20:48:53 +0100

    Added printing functions, added more university-country pairs

--- main.py
+++ main.py
@@ -12,14 +12,24 @@ def argument_parser():
   
   return parser
   
+  
+def PointsCalculation(n_of_teams, place, maxProblemsSolved, problemsSolved):
+    points = math.pow((n_of_teams-place+1)/float(n_of_teams) * (problemsSolved/maxProblemsSolved),1)
+    return points
+  
             
 def CalculatePointsPerInstitution(table,overall, factor=1,printTop3=True):
     thisYearInst = {}
+    
+    maxProblemSolved = -1
      
     for item in table:
         institution = item[0]
         place = item[1]
-        points = math.pow((len(table)-int(place)+1)/float(len(table)),2)
+        if maxProblemSolved == -1:
+            maxProblemSolved = item[2]
+        
+        points = PointsCalculation(len(table),float(place),float(maxProblemSolved),float(item[2]))
         if institution not in thisYearInst.keys():
             thisYearInst[institution] = [points,1]
         else:
@@ -45,14 +55,60 @@ def CalculatePointsPerInstitution(table,overall, factor=1,printTop3=True):
 
 def CalculatePointsPerCountry(table,overall, factor=1, printTop3=True):
     thisYearCountry = {}
+    
+    maxProblemSolved = -1
 
     for item in table:
         institution = item[0]
         place = item[1]
+        if maxProblemSolved == -1:
+            maxProblemSolved = item[2]
+
         
         if institution in insitution_to_country.keys():
-       	    points = math.pow((len(table)-int(place)+1)/float(len(table)),2)
+       	    points = PointsCalculation(len(table),float(place),float(maxProblemSolved),float(item[2]))
             country = insitution_to_country[institution]
+            if country not in thisYearCountry.keys():
+            	thisYearCountry[country] = [points,1]
+            else:
+            	thisYearCountry[country][0] += points
+            	thisYearCountry[country][1] += 1
+
+        
+    arrCountries = []
+    for key in thisYearCountry.keys():
+        arrCountries.append([thisYearCountry[key][0]/float(math.sqrt(thisYearCountry[key][1])),key])
+    
+    
+    arrCountries.sort(reverse=True)
+    if printTop3:
+        for i in range(3):
+            print(str(i+1)+'.',arrCountries[i][1],'   points:',arrCountries[i][0])
+        print()
+            
+    for item in arrCountries:
+        if item[1] not in overall.keys():
+    	    overall[item[1]] = item[0]*factor
+        else:
+            overall[item[1]] += item[0] * factor
+            
+def CalculatePointsPerRegion(table,overall, factor=1, printTop3=True):
+    thisYearCountry = {}
+    
+    maxProblemSolved = -1
+
+    for item in table:
+        institution = item[0]
+        place = item[1]
+        if maxProblemSolved == -1:
+            maxProblemSolved = item[2]
+
+        
+        if institution in insitution_to_country.keys():
+       	    points = PointsCalculation(len(table),float(place),float(maxProblemSolved),float(item[2]))
+            country = insitution_to_country[institution]
+            if country in countries_to_regions_EU.keys():
+                country = countries_to_regions_EU[country]
             if country not in thisYearCountry.keys():
             	thisYearCountry[country] = [points,1]
             else:
@@ -81,6 +137,7 @@ def Run(region,verbose):
     # institution, points
     overallPoints = {}
     overallPointsCountry = {}
+    overallPointsRegion = {}
     year = int(date.today().year)
     if True:
         for i in range(0, 10):
@@ -91,28 +148,17 @@ def Run(region,verbose):
                 CalculatePointsPerCountry(table, overallPointsCountry, (i + 1) / 10.0)
             else:
                 CalculatePointsPerCountry(table, overallPointsCountry, (i + 1) / 10.0,False)
+                
+            if verbose=='True':
+                CalculatePointsPerRegion(table, overallPointsRegion, (i + 1) / 10.0)
+            else:
+                CalculatePointsPerRegion(table, overallPointsRegion, (i + 1) / 10.0,False)
 
         # Final print
-        arr = []
-        for key in overallPoints.keys():
-            arr.append([overallPoints[key], key])
-        arr.sort(reverse=True)
-
-        print('Overall:')
-        for i in range(len(arr)):
-            if i>=100 and verbose=='False':
-                continue
-            print(str(i + 1) + '.', arr[i][1], '   points:', arr[i][0])
-            
-        arrCountries = []
-        for key in overallPointsCountry.keys():
-            arrCountries.append([overallPointsCountry[key], key])
-        arrCountries.sort(reverse=True)
-        
-        print()
-       	print('Overall Countries:')
-        for i in range(len(arrCountries)):
-            print(str(i + 1) + '.', arrCountries[i][1], '   points:', arrCountries[i][0])
+        PrintUniRankings(overallPoints,verbose)
+        PrintCountryRankings(overallPointsCountry,verbose)
+        PrintRegionRankings(overallPointsRegion, verbose)
+        PrintUniInRegionrRanking(overallPoints,'EU',verbose)
           
     #except Exception as ex:
     #    print(ex)
--- utils.py
+++ utils.py
@@ -13,10 +13,79 @@ insitution_to_country = {'St. Petersburg ITMO University':'Russia','University o
 			'Saratov State University':'Russia','Ural Federal University':'Russia','Jagiellonian University in Krakow':'Poland',
 			'National Research University Higher School of Economics':'Russia','KAIST':'South Korea','University of Bucharest':'Romania',
 			'ETH Zürich':'Switzerland','Stanford University':'USA','Beihang University':'China',
-			'Canada':'University of Waterloo','Zhongshan (Sun Yat-sen) University':'China','The Chinese University of Hong Kong':'Hong Kong',
-			'University of Zagreb':'Croatia'}
+			'University of Waterloo':'Canada','Zhongshan (Sun Yat-sen) University':'China','The Chinese University of Hong Kong':'Hong Kong',
+			'University of Zagreb':'Croatia','Lviv National University':'Ukraine','University of Cambridge':'United Kingdom',
+'Nizhny Novgorod State University':'Russia',
+			'UNSW Sydney':'Australia','University of Helsinki':'Finland',
+                                                               'Zhejiang University': 'China',
+                                                                                        'National University of Singapore': 'Singapore',
+'University of Electronic Science and Technology of China': 'China',
+'University of California, Berkeley': 'USA',
+'Carnegie Mellon University': 'USA',
+'Taras Shevchenko National University of Kyiv': 'Ukraine',
+'University of Engineering and Technology - VNU': 'Vietnam',
+'Belarusian State University of Informatics and Radioelectronics': 'Belarus',
+'Universidad de Buenos Aires - FCEN': 'Brazil',
+'St. Petersburg Academic University': 'Russia',
+'Korea University': 'South Korea',
+'The University of British Columbia': 'Canada',
+'University of Latvia': 'Latvia',
+'University of Southern California': 'USA',
+'Perm State University': 'Russia',
+'Fuzhou University': 'China',
+'Universidad Nacional de Córdoba - FaMAF': 'Argentina',
+'Kyoto University': 'Japan',
+'Universidade Federal de Pernambuco': 'Brazil',
+'Universitas Indonesia': 'Indonesia',
+'University of Illinois at Urbana-Champaign': 'USA',
+'Universitat Politècnica de Catalunya': 'Spain',
+'Bangladesh University of Engineering and Technology': 'Bangladesh',
+'Universidade de São Paulo': 'Brazil',
+'Novosibirsk State University': 'Russia',
+'University of Wisconsin-Madison': 'USA',
+'International Institute of Information Technology - Hyderabad': 'India',
+'Moscow Aviation Institute': 'Russia',
+'Beijing Normal University': 'China',
+'South China University of Technology': 'China',
+'Hangzhou Dianzi University': 'China',
+'University of Oxford': 'United Kingdom',
+'Vilnius University': 'Lithuania',
+'Indian Institute of Technology - Bombay': 'India',
+'École Normale Supérieure de Paris': 'France',
+'Charles University in Prague': 'Czechia',
+'Indian Institute of Technology - Roorkee': 'India',
+'Indian Institute of Technology - Delhi': 'India',
+'KTH - Royal Institute of Technology': 'Sweden',
+'Universidad Nacional de Rosario': 'Argentina',
+'Scuola Normale Superiore': 'Italy',
+'National Yang Ming Chiao Tung University': 'Taiwan',
+'University of Tsukuba': 'Japan',
+'Indian Institute of Technology - Kanpur': 'India',
+'University of Michigan at Ann Arbor': 'USA',
+'Technische Universität München': 'Germany',
+'Kharkiv National University of Radio Electronics': 'Ukraine',
+'Cornell University': 'USA',
+'The University of Texas at Austin': 'USA',
+'Beijing University of Posts and Telecommunications': 'China',
+'Institut Teknologi Bandung': 'Indonesia',
+'St. Petersburg Campus of Higher School of Economics': 'Russia',
+'Tokyo Institute of Technology': 'Japan',
+'University of Aizu': 'Japan',
+'Shanghai University': 'China',
+'Universidade de São Paulo - Campus de São Carlos': 'Brazil',
+'Innopolis University': 'Russia',
+'Comenius University': 'Slovakia',
+'International IT University': 'Kazakhstan',
+'Columbia University': 'USA',
+}
 			
-countries_to_regions = {'Poland':'EU','Romania':'EU','Croatia':'EU'}
+countries_to_regions_EU = {'Poland':'EU','Romania':'EU','Croatia':'EU','Finland':'EU','Latvia':'EU','Spain':'EU','Lithuania':'EU','France':'EU','Czechia':'EU',
+                            'Sweden':'EU','Italy':'EU','Germany':'EU','Slovakia':'EU'}
+
+countries_to_regions_whole = {'Poland':'Europe','Romania':'Europe','Croatia':'Europe','Finland':'Europe','Russia':'Russia','China':'Asia','South Korea':'Asia','Japan':'Asia','Belarus':'Europe','USA':'North America',
+			      'Australia':'Asia','Canada':'North America','United Kingdom':'Europe','Switzerland':'Europe','Taiwan':'Asia','Hong Kong':'Asia','Iran':'Asia','Ukraine':'Europe','Singapore':'Asia',
+                              'Vietnam':'Asia','Brazil':'South America','Latvia':'Europe','Spain':'Europe','Indonesia':'Asia','Argentina':'South America','Bangladesh':'Asia','India':'Asia'
+                              ,'Lithuania':'Europe','France':'Europe','Czechia':'Europe','Sweden':'Europe','Italy':'Europe','Germany':'Europe','Slovakia':'Europe','Kazakhstan':'Asia'}
 
 
 
@@ -51,6 +120,75 @@ def getTable(contestID):
         counter += 1
         place = counter
         institution = item['institution']
-        table.append([institution,place])
+        problemSolved = item['problemsSolved']
+        if problemSolved==None:
+           problemSolved = 1.0
+        table.append([institution,place,problemSolved])
         
     return table
+
+def PrintUniRankings(rankings, verbose):
+    arr = []
+    for key in rankings.keys():
+        arr.append([rankings[key], key])
+    arr.sort(reverse=True)
+
+    print('Overall:')
+    for i in range(len(arr)):
+        if i >= 100 and verbose == 'False':
+            continue
+        print(str(i + 1) + '.', arr[i][1], '   points:', arr[i][0])
+        # print('\'',arr[i][1], '\':', '\'\',')
+
+
+def PrintCountryRankings(rankings, verbose):
+    arrCountries = []
+    for key in rankings.keys():
+        arrCountries.append([rankings[key], key])
+    arrCountries.sort(reverse=True)
+
+    print()
+    print('Overall Countries:')
+
+
+    for i in range(len(arrCountries)):
+        print(str(i + 1) + '.', arrCountries[i][1], '   points:', arrCountries[i][0])
+
+
+def PrintRegionRankings(rankings, verbose):
+    arrRegions = []
+    for key in rankings.keys():
+        arrRegions.append([rankings[key], key])
+    arrRegions.sort(reverse=True)
+
+    print()
+    print('Overall Regions:')
+
+
+    for i in range(len(arrRegions)):
+        print(str(i + 1) + '.', arrRegions[i][1], '   points:', arrRegions[i][0])
+
+def PrintUniInRegionrRanking(rankings, regionName, verbose):
+    arr = []
+    for key in rankings.keys():
+        arr.append([rankings[key], key])
+    arr.sort(reverse=True)
+
+    if regionName == 'EU':
+        print('Overall ',regionName,':')
+        counter = 0
+        for i in range(len(arr)):
+            if arr[i][1] in insitution_to_country.keys():
+                country = insitution_to_country[arr[i][1]]
+                if country in countries_to_regions_EU.keys():
+                    print(str(counter + 1) + '.', arr[i][1], '   points:', arr[i][0])
+                    counter += 1
+    else:
+        print('Overall ',regionName,':')
+        counter = 0
+        for i in range(len(arr)):
+            if arr[i][1] in insitution_to_country.keys():
+                country = insitution_to_country[arr[i][1]]
+                if countries_to_regions_whole[country] == regionName:
+                    print(str(counter + 1) + '.', arr[i][1], '   points:', arr[i][0])
+                    counter += 1
